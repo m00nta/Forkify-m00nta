e6f3dc8927b6516e12d8f547947dba4a1895e2de
m00nta <mosbehimontassar@gmail.com>
2021-12-13 13:20:36 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,4 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'baaa8526-5076-41d6-a1bd-4c440b649037';
